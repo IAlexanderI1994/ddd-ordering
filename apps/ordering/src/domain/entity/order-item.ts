f9724743fe271d1711b5b5306e05955a01188e4c
Alexander Kiryushin <kontaktAK@yandex.ru>
2022-08-26 23:40:44 +0300

    orderitem builder

--- apps/ordering/src/domain/entity/order-item.ts
+++ apps/ordering/src/domain/entity/order-item.ts
@@ -1,10 +1,109 @@
-import {BaseEntity, OrderId, OrderItemId} from "@ordering/common/domain";
+import {BaseEntity, Money, OrderId, OrderItemId} from "@ordering/common/domain";
 import {Product} from "./product";
 
-export class OrderItem extends BaseEntity<OrderItemId>{
+export class OrderItem extends BaseEntity<OrderItemId> {
 
-  private readonly orderId: OrderId;
+  get orderId(): OrderId {
+    return this._orderId;
+  }
 
-  private readonly product: Product;
+  get product(): Product {
+    return this._product;
+  }
+
+  get quantity(): number {
+    return this._quantity;
+  }
+
+  get price(): Money {
+    return this._price;
+  }
+
+  get subtotal(): Money {
+    return this._subtotal;
+  }
+
+  private readonly _orderId: OrderId;
+
+  private readonly _product: Product;
+
+  private readonly _quantity: number;
+
+  private readonly _price: Money;
+
+  private readonly _subtotal: Money;
+
+  private constructor(builder: typeof OrderItem.Builder.prototype) {
+    super();
+    super.setId(builder.orderItemId)
+    this._product = builder.product
+    this._quantity = builder.quantity
+    this._price = builder.price
+    this._subtotal = builder.subtotal
+
+  }
+
+  static builder(): typeof OrderItem.Builder.prototype {
+    return new OrderItem.Builder();
+  }
+
+  static Builder = class {
+
+    private _orderItemId: OrderItemId;
+    private _product: Product;
+    private _quantity: number;
+    private _price: Money;
+    private _subtotal: Money;
+
+    get orderItemId(): OrderItemId {
+      return this._orderItemId;
+    }
+
+    get product(): Product {
+      return this._product;
+    }
+
+    get quantity(): number {
+      return this._quantity;
+    }
+
+    get price(): Money {
+      return this._price;
+    }
+
+    get subtotal(): Money {
+      return this._subtotal;
+    }
+
+    setOrderItemId(value: OrderItemId): typeof OrderItem.Builder.prototype {
+      this._orderItemId = value;
+      return this;
+    }
+
+    setProduct(value: Product): typeof OrderItem.Builder.prototype {
+      this._product = value;
+      return this;
+    }
+
+    setQuantity(value: number): typeof OrderItem.Builder.prototype {
+      this._quantity = value;
+      return this;
+    }
+
+    setPrice(value: Money): typeof OrderItem.Builder.prototype {
+      this._price = value;
+      return this;
+    }
+
+    setSubtotal(value: Money): typeof OrderItem.Builder.prototype {
+      this._subtotal = value;
+      return this;
+    }
+
+    build(): OrderItem {
+      return new OrderItem(this)
+    }
+
+  }
 
 }
--- apps/ordering/src/domain/entity/product.ts
+++ apps/ordering/src/domain/entity/product.ts
@@ -1,12 +1,27 @@
 import {
-  AggregateRoot,
+  BaseEntity, Money,
   ProductId,
 } from "@ordering/common/domain";
 
-export class Product extends AggregateRoot<ProductId> {
+export class Product extends BaseEntity<ProductId> {
 
+  get name(): string {
+    return this._name;
+  }
 
+  get price(): Money {
+    return this._price;
+  }
 
+  private readonly _name: string;
+  private readonly _price: Money;
+
+  constructor(productId: ProductId, name: string, price: Money) {
+    super();
+    super.setId(productId)
+    this._name = name;
+    this._price = price;
+  }
 
 
 }
