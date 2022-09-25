746aaf0d9d530b09008bbfd3252771183ee6e0d3
Alexander Kiryushin <kontaktAK@yandex.ru>
2022-09-25 22:08:44 +0500

    kafka infra init first avro schema

---
+++ libs/infra/kafka/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'infra-kafka',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/infra/kafka',
+};
